1021c9895f3c9a427946a869dad6e3e24b247189
Abhishek Agarwal <agabhi001@gmail.com>
2020-11-17 17:00:29 +0530

    second commit

--- Week 2/fib_sum_last_digit_v0.py
+++ Week 2/fib_sum_last_digit_v0.py
@@ -13,13 +13,24 @@ def fib_array(n) :
     
     return array[n]
 
-def fib_sum(n):
+def fib_sum_recurse(n):
     
     if n <= 1:
         return fib_array(n)
     
-    return fib_sum(n - 1) + fib_sum(n - 2) + 1
+    return fib_sum_recurse(n - 1) + fib_sum_recurse(n - 2) + 1
+
+'''
+(F0 + F1 + F2 + .... + Fn) % 10 = F0 % 10 + F1 % 10 + .... + Fn % 10
+
+Using pisano period, Fn % 10 = F(n % 60) as 60 is the pisano period of 10
+'''
+
+def fib_sum_pisano(n) :
+    rem1 = n % 60
+    rem2 = (n + 1) % 60
+    return (fib_array(rem1) + fib_array(rem2) - 1) % 10
 
 if __name__ == "__main__" :
     n = int(input())
-    print(fib_sum(n))
+    print(fib_sum_pisano(n))
